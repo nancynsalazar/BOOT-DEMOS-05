e8cfd5e007c71bac3ef4c483560be6e47546d321
var name = 'Nancy <nancy_salazar@outlook.com>
2023-01-02 18:24:31 -0600

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose")
 const connectDB = async () => {
 
     try {
+        mongosee.set("strictQuery", false);
         await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
